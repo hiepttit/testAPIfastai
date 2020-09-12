e3dd6abdde183ac11f27d9e669747c2d8c056262
hieptt <hiepttctk40@gmail.com>
2020-09-12 11:21:02 +0700

    v2

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from fastai import *
+from fastai.vision.all import *
+from fastai.data.external import untar_data,URLs
+from fastai.data.transforms import get_image_files
+from flask_cors import CORS,cross_origin
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+# load the learner
+learn = load_learner('export.pkl')
+classes = learn.dls.vocab
+
+
+def predict_single(img_file):
+    'function to take image and return prediction'
+    prediction = learn.predict(img_file)
+    probs_list = prediction[2].numpy()
+    return {
+        'category': classes[prediction[1].item()],
+        'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
+    }
+
+
+# route for prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    return jsonify(predict_single(request.files['image']))
+
+if __name__ == '__main__':
+    app.run()
